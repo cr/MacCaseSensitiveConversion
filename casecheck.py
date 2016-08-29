a5517a97ba360aa2f6fb400ad12aad63dc7cbbc9
Christiane Ruetten <cr@23bit.net>
2016-08-29 16:21:28 +0200

    Update casecheck.py

--- casecheck.py
+++ casecheck.py
@@ -2,15 +2,10 @@
 
 import os
 
-def is_casedupe(name, names):
-	for n in names:
-		if n != name:
-			if n.lower() == name.lower():
-				return True
-	return False
-
 for root, dirs, files in os.walk("."):
 	content = dirs + files
-	for entry in content:
-		if is_casedupe(entry, content):
-			print(root + "/" + entry)
+	for one in content:
+		for other in content:
+			if one != other:
+				if one.lower() == other.lower():
+					print(root + "/" + one)
