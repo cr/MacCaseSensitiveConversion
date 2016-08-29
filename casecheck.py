0e1217ca5a4b3bfa19b2cc4776edd27e406d96ed
Christiane Ruetten <cr@23bit.net>
2016-08-29 16:06:43 +0200

    Create casecheck.py

---
+++ casecheck.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+import os
+
+def is_casedupe(name, names):
+	for n in names:
+		if n != name:
+			if n.lower() == name.lower():
+				return True
+	return False
+
+for root, dirs, files in os.walk("."):
+	content = dirs + files
+	for entry in content:
+		if is_casedupe(entry, content):
+			print(root+'/'+a)
